40f6599e12a270a8b7a4c22aee21d7bbef7e35fb
Jim Garrett <jimgarrett123@gmail.com>
2018-09-05 21:14:01 -0400

    new file

--- tests/app_test.js
+++ tests/app_test.js
@@ -1,4 +1,4 @@
-var server   = require('../server'),
+var server   = require('../app'),
     chai     = require('chai'),
     chaiHTTP = require('chai-http'),
     should   = chai.should();
@@ -18,4 +18,4 @@ describe('Basic routes tests', function() {
         })
 
     })
-})
+067504})
